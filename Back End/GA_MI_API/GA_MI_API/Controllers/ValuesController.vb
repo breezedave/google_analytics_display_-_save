0f2352177951caeedcb69dd2ce0adbb8f66a1766
Dave Breeze <breezedave@googlemail.com>
2014-06-17 18:28:02 +0100

    Need list pulled. Each individual request pulled from analytics & posted to apis. Api doesn't yet save the posted data

--- Back End/GA_MI_API/GA_MI_API/Controllers/ValuesController.vb
+++ Back End/GA_MI_API/GA_MI_API/Controllers/ValuesController.vb
@@ -12,23 +12,9 @@ Public Class ValuesController
         Return str
     End Function
 
-    ' GET api/values/5
-    Public Function GetValue(ByVal id As Integer) As String
-        Return "value"
+    ' POST api/values
+    Public Function PostValue(<FromBody()> ByVal value As String) As String
+        Return "Ok"
     End Function
 
-    ' POST api/values
-    Public Sub PostValue(<FromBody()> ByVal value As String)
-
-    End Sub
-
-    ' PUT api/values/5
-    Public Sub PutValue(ByVal id As Integer, <FromBody()> ByVal value As String)
-
-    End Sub
-
-    ' DELETE api/values/5
-    Public Sub DeleteValue(ByVal id As Integer)
-
-    End Sub
 End Class
--- Back End/GA_MI_API/GA_MI_API/Scripts/getData.js
+++ Back End/GA_MI_API/GA_MI_API/Scripts/getData.js
@@ -7,6 +7,20 @@ getData.queryingGA = false;
 getData.loop = function () {
     getData.looping = setInterval(getData.isReady, 500);
 }
+getData.needListPass = function () {
+    clearInterval(getData.looping);
+    if (getData.needList.length > 0) {
+        var params = {};
+        params.profileId = getData.profile;
+        params.real_date = getData.needList[0].real_date.substr(0,10);
+        params.metric = getData.needList[0].metric;
+        getData.needList.splice(0, 1);
+        getData.pullFromGoogle(params);
+    } else {
+        alert("needlist empty");
+    }
+}
+
 getData.isReady = function () {
     if (getData.progress == 0) {
         if (getData.account != "" && getData.profile != "") { getData.ready = true; getData.progress++}
@@ -16,26 +30,28 @@ getData.isReady = function () {
     }
 
     if (getData.progress == 2) {
-        //practice query
-        if (getData.queryingGA == false) {
-            queryCoreReportingApi(getData.profile);
-            getData.queryingGA == true;
-        }
+        getData.needListPass();
     }
 
-    if (getData.progress == 3) {
-        clearInterval(getData.looping);
-        var json = JSON.stringify(getData.results);
-        alert(json.toString());
-        dsdConn.post(json);
-    }
     if (getData.progress == 5) {
         clearInterval(getData.looping);
         alert('Grabbing data from DSD failed');
     }
 
 }
+getData.pullFromGoogle = function (params) {
+    var metricAdd = "";
+    if(params.metric=='ga:pageviews'){metricAdd = ',ga:pagepath'}
+    gapi.client.analytics.data.ga.get({
+        'ids': 'ga:' + params.profileId
+        , 'start-date': params.real_date
+        , 'end-date': params.real_date
+        , 'dimensions': 'ga:year,ga:month,ga:day,ga:hour' + metricAdd
+        , 'metrics': params.metric
+    }).execute(printResults);
+}
 
+/*
 function queryCoreReportingApi(profileId) {
     console.log('Querying Core Reporting API.');
 
@@ -48,17 +64,19 @@ function queryCoreReportingApi(profileId) {
         ,'metrics': 'ga:pageviews'
     }).execute(printResults);
 }
+*/
 
+var a;
 function printResults(results) {
     if (results.error) {
-        Alert('Something went ary, with my Canada Dry')
+        a = results.error;
+        alert(results.error);
     } else {
         if (results.rows && results.rows.length) {
             var records = [];
             for (var i = 0; i < results.rows.length; i++) {
                 var row = [];
                 for (var i2 = 0; i2 < results.rows[i].length; i2++) {
-                    console.log(results.rows[i][i2]);
                     row.push(results.rows[i][i2]);
                 }
                 records.push(row);
@@ -68,11 +86,28 @@ function printResults(results) {
         }
     }
     getData.results = records;
-    getData.progress++;
-    getData.queryingGA = false;
+    var json = JSON.stringify(getData.results);
+    dsdConn.post(json);
 }
 
 var dsdConn = {};
+dsdConn.get = function () {
+    var xmlHTTP;
+    if (window.XMLHttpRequest) {
+        xmlHTTP = new window.XMLHttpRequest;
+    } else {
+        alert("This service will not work with your browser");
+    }
+    xmlHTTP.open("GET", "api/values", true);
+    xmlHTTP.send();
+    xmlHTTP.onreadystatechange = function () {
+        if (xmlHTTP.readyState == 4 && xmlHTTP.status == 200) {
+            getData.needList = eval( "(" + xmlHTTP.response + ")");
+            getData.progress++
+        }
+        if (xmlHTTP.readyState == 4 && xmlHTTP.status != 200) { alert(xmlHTTP.status); getData.progress = 5 }
+    }
+}
 dsdConn.post = function (json) {
     var xmlHTTP;
     if (window.XMLHttpRequest) {
@@ -86,22 +121,14 @@ dsdConn.post = function (json) {
     xmlHTTP.send(json);
 
     xmlHTTP.onreadystatechange = function () {
-        if (xmlHTTP.readyState == 4 && xmlHTTP.status == 200) { alert("Worked") }
-        if (xmlHTTP.readyState == 4 && xmlHTTP.status != 200) { alert(xmlHTTP.status) }
-    }
-}
-dsdConn.get = function () {
-    var xmlHTTP;
-    if (window.XMLHttpRequest) {
-        xmlHTTP = new window.XMLHttpRequest;
-    } else {
-        alert("This service will not work with your browser");
-    }
-    xmlHTTP.open("GET", "api/values", true);
-    xmlHTTP.send();
-    xmlHTTP.onreadystatechange = function () {
-        if (xmlHTTP.readyState == 4 && xmlHTTP.status == 200) { alert("Worked"); getData.progress++ }
-        if (xmlHTTP.readyState == 4 && xmlHTTP.status != 200) { getData.progress = 5}
+        if (xmlHTTP.readyState == 4 && xmlHTTP.status == 200) { 
+            console.log(xmlHTTP.status);
+            getData.needListPass();
+        }
+        if (xmlHTTP.readyState == 4 && xmlHTTP.status != 200) { 
+            console.log(xmlHTTP.status + " " + xmlHTTP.statusText);
+            getData.needListPass();
+        }
     }
 }
 
