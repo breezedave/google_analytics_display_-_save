10b776d3b4c112830f319b18a8ef543e46cc7dd4
Dave Breeze <breezedave@googlemail.com>
2014-06-17 13:55:58 +0100

    Google Authentication working. Currently only one dummy query

---
+++ Front End/GA_MI/js/auth_api_v3.js
@@ -0,0 +1,62 @@
+﻿var clientId = '322876015263-24nc1splan07oqm0gni9nu8rnohf60go.apps.googleusercontent.com';
+var apiKey = 'AIzaSyC_8dWztKhJ-f7ny-m7jOeBXZ0gfqxhMqk';
+var scopes = 'https://www.googleapis.com/auth/analytics.readonly';
+var authTried = false;
+
+// This function is called after the Client Library has finished loading
+function handleClientLoad() {
+    gapi.client.setApiKey(apiKey);
+    window.setTimeout(checkAuth, 1);
+}
+
+
+function checkAuth() {
+    gapi.auth.authorize({ client_id: clientId, scope: scopes, immediate: true }, handleAuthResult);
+}
+
+
+function handleAuthResult(authResult) {
+    if (authTried) {
+        // The user has authorized access
+        // Load the Analytics Client. This function is defined in the next section.
+        loadAnalyticsClient();
+    } else {
+        // User has not Authenticated and Authorized
+        handleUnAuthorized();
+        authTried=true;
+    }
+}
+
+
+// Authorized user
+function handleAuthorized() {
+    var authorizeButton = document.getElementById('authorize-button');
+    var makeApiCallButton = document.getElementById('make-api-call-button');
+
+    //makeApiCallButton.style.visibility = '';
+    authorizeButton.style.visibility = 'hidden';
+    makeApiCall()
+}
+
+
+// Unauthorized user
+function handleUnAuthorized() {
+    var authorizeButton = document.getElementById('authorize-button');
+    var makeApiCallButton = document.getElementById('make-api-call-button');
+
+    makeApiCallButton.style.visibility = 'hidden';
+    authorizeButton.style.visibility = '';
+    authorizeButton.onclick = handleAuthClick;
+}
+
+
+function handleAuthClick(event) {
+    gapi.auth.authorize({ client_id: clientId, scope: scopes, immediate: false }, handleAuthResult);
+    return false;
+}
+
+
+function loadAnalyticsClient() {
+    // Load the Analytics client and set handleAuthorized as the callback function
+    gapi.client.load('analytics', 'v3', handleAuthorized);
+}
---
+++ Front End/GA_MI/js/getData.js
@@ -0,0 +1,55 @@
+﻿var getData = {};
+getData.ready = false;
+getData.account = "";
+getData.profile = "";
+getData.progress = 0; // 0 = authenticating, 1 = getting need/want list, 2= getting data, 3=resting
+getData.loop = function () {
+    getData.looping = setInterval(getData.isReady, 500);
+}
+getData.isReady = function () {
+    if (getData.progress == 0) {
+        if (getData.account != "" && getData.profile != "") { getData.ready = true; getData.progress++}
+    }
+    if (getData.progress == 1) {
+        //practice query
+        queryCoreReportingApi(getData.profile);
+    }
+    if (getData.progress == 2) {
+        clearInterval(getData.looping);
+        alert(getData.results);
+    }
+}
+
+function queryCoreReportingApi(profileId) {
+    console.log('Querying Core Reporting API.');
+
+    // Use the Analytics Service Object to query the Core Reporting API
+    gapi.client.analytics.data.ga.get({
+        'ids': 'ga:' + profileId,
+        'start-date': '2014-03-03',
+        'end-date': '2014-04-03',
+        'metrics': 'ga:sessions'
+    }).execute(printResults);
+}
+
+function printResults(results) {
+    if (results.error) {
+        Alert('Something went ary, with my Canada Dry')
+    } else {
+        if (results.rows && results.rows.length) {
+            for (var i = 0; i < results.rows.length; i++) {
+                for (var i2 = 0; i2 < results.rows[i].length; i2++) {
+                    console.log(results.rows[i][i2]);
+                    getData.results = results.rows[i][i2];
+                }
+            }
+            getData.progress++;
+        } else {
+            console.log('No results found');
+        }
+    }
+}
+
+
+
+getData.loop();
